35ece84c5a2558104d42b6bc897d246d00ec1406
jleon1994 <juannicolasleonruiz@gmail.com.com>
2024-10-24 20:27:23 -0500

    Configuración inicial del proyecto

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
+
+  
