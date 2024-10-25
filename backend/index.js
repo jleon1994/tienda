781284b60d0906bdf2ea1056e2056d2c8a23473f
jleon1994 <juannicolasleonruiz@gmail.com.com>
2024-10-24 22:37:12 -0500

    Modulo index inicial

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 
+
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Conectado a MongoDB Atlas'))
@@ -23,6 +24,8 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
+
 
 
   
