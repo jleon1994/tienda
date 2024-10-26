7c12664fe5939d56ecfaeea7feb90f39a8dc45ef
jleon1994 <juannicolasleonruiz@gmail.com.com>
2024-10-25 21:17:57 -0500

    Configuración de Railway y MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
 const authRoutes = require('./routes/auth');
 
 dotenv.config();
 const app = express();
-app.use(express.json()); // Parsear JSON
+app.use(express.json());
 
 const path = require('path');
 
 // Servir archivos estáticos (frontend)
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-
-
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Conectado a MongoDB Atlas'))
     .catch(err => console.error('Error en la conexión:', err));
+
+// Servir archivos estáticos (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Rutas
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+
 
 
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -21,3 +21,4 @@ const UserSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', UserSchema);
+
